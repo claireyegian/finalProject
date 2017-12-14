14f6761f43bd6297179686b61ca73dbf4f444430
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-12-14 14:50:39 -0500

    Update gameOfLife.py

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,1 +1,23 @@
+#Claire Yegian
+#12/14/17
+#gameOfLife.py - creates Conway's Game of Life
 
+from ggame import *
+
+def buildBoard():
+    gameBoard =  [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
+    rowNum = 1
+    for row in gameBoard:
+        columnNum = 1
+        for column in row:
+            Sprite(data['deadCell'],((rowNum*50)-25,(columnNum*50)-25))
+        columnNum += 1
+    rowNum += 1
+
+if __name__ == '__main__':
+    dead = Color(0xffffff,1)
+    live = Color(0x000000,1)
+    
+    data = {}
+    data['deadCell'] = RectangleAsset(50,50,LineStyle(1,dead),dead)
+    data['liveCell'] = RectangleAsset(50,50,LineStyle(1,live),live)
